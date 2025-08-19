7552aca8dd25e141e3d713bef970227e04e49b94
Moynul7 <hmoynul018@gmail.com>
2025-08-19 16:06:24 +0600

    Create JavaScript event

--- Tour of DOM/js/event.js
+++ Tour of DOM/js/event.js
@@ -14,9 +14,54 @@ function makeBlue(){
     document.body.style.backgroundColor = 'blue';
 }
 
+//option 3 another onclick function
 
 const makePurpleBtn = document.getElementById('purple');
 makePurpleBtn.onclick = function(){
     document.body.style.backgroundColor = 'purple';
 }
 
+
+//option 4 
+
+const makeButtonPink = document.getElementById('pink');
+// makeButtonPink.addEventListener('click', makePink);
+// function makePink(){
+//     document.body.style.backgroundColor = 'pink';
+// };
+
+// option 4 another
+
+makeButtonPink.addEventListener('click', function makePink(){
+    document.body.style.backgroundColor = 'pink';
+});
+
+// option 5
+
+document.getElementById('Orange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange';
+})
+
+
+//handler function
+
+function eventHandler() {
+    const handlerStatus = document.getElementById('Handler-Status');
+    handlerStatus.innerText = "`I'm` Working Properly";
+}
+// Listener Function
+const changeListner = document.getElementById('change-listner');
+changeListner.addEventListener('click', function(){
+    const listnerStatus = document.getElementById('Listner-Status');
+    listnerStatus.innerText = 'Text change by Lister Function';
+})
+
+const updatedButton = document.getElementById('updated');
+updatedButton.addEventListener('click', function(){
+    const inputField = document.getElementById('input-field');
+    const inputText = inputField.value;
+    
+    const displayText = document.getElementById('input-text-display');
+    displayText.innerText = inputText;
+    inputField.value = '';
+})
